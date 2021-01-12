f7fd184f53cca2220a7139dab5570eef429cb5b7
Bai794 <1269724114@qq.com>
2021-01-12 22:35:34 +0800

    first commit

---
+++ MDK-ARM/RTE/RTE_Components.h
@@ -0,0 +1,14 @@
+
+/*
+ * Auto generated Run-Time-Environment Component Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'gongcheng' 
+ * Target:  'gongcheng' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+#endif /* RTE_COMPONENTS_H */
